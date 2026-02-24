a617844b737e40161c765d2efbc08e0a4e821d38
preetshah029 <preetshah029@gmail.com>
2026-02-24 15:01:51 +0530

    relations

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 class Customer extends Model
 {
+    protected $fillable = ['name', 'email', 'phone', 'address'];
     public function orders(): HasMany
    {
        return $this->hasMany(Order::class);
